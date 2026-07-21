4c2ec496b94a596e64400f001e51a13ca1b719a1
Office JC <office.jachanland@gmail.com>
2026-07-21 14:44:22 +0700

    Configure Apps Script endpoint

--- js/api-config.js
+++ js/api-config.js
@@ -2,4 +2,4 @@
 //  ตั้งค่า Google Apps Script Web App URL
 //  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
 // ============================================================
-const GAS_API_URL = '';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbzkQN38o0hW4ZW-1e8JoQHZYkQikgWT4VZNAc6CuvpOoAKyDEUsjS8CZBOPjZQ0TEsBMw/exec';
